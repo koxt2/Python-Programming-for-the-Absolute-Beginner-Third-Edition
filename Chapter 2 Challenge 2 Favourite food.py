4faeec9213e454ff1c095b87c69d7d263ed59b17
koxt2 <58732688+koxt2@users.noreply.github.com>
2019-12-10 12:40:01 +0000

    Add files via upload
    # Python programming for the Absolute Beginner, Third Edition
    # Chapter 2, Challenge 3
    
    # Write a program that allows a user to enter his or her two favourite foods. 
    # The program should then print out the name of a new food by joining the original food names together.

---
+++ Chapter 2 Challenge 2 Favourite food.py
@@ -0,0 +1,10 @@
+# Python programming for the Absolute Beginner, Third Edition
+# Chapter 2, Challenge 3
+
+# Write a program that allows a user to enter his or her two favourite foods. 
+# The program should then print out the name of a new food by joining the original food names together. 
+
+#Favourite food
+meat = input("Enter the name of your favourite meat?:\n")
+veg = input("Enter the name of your favourite veg?:\n")
+print("You like", meat, "and", veg, "!")
