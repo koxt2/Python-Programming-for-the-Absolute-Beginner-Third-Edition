c07b89bc383d9c6ae6e7c49649ba8deefe6ea4f0
koxt2 <58732688+koxt2@users.noreply.github.com>
2019-12-10 12:36:29 +0000

    Add files via upload
    # Python programming for the Absolute Beginner, Third Edition
    # Chapter 1, Challenge 2
    
    # Create a list of legal and illegal variable names. Describe why each is either legal or illegal.
    # Next, create a list of "good" and "bad" legal variables names. Describe why each is either a good or bad choice for a variable name.

---
+++ Chapter 2 Challenge 1 Legal an illegal variable names.py
@@ -0,0 +1,20 @@
+# Python programming for the Absolute Beginner, Third Edition
+# Chapter 1, Challenge 2
+
+# Create a list of legal and illegal variable names. Describe why each is either legal or illegal.
+# Next, create a list of "good" and "bad" legal variables names. Describe why each is either a good or bad choice for a variable name. 
+
+#legal variable names
+# limit
+# limit_hi
+# limit_1
+
+#Illegal variable names
+# 1_limit 		- starts with a number
+
+#Poor variable names
+# limit_46246141346 	- complicated
+# Limit 		- Starts with a capital letter
+# LIMIT 		- In capitals (though not so bad if used as a constant
+# s 			- Not descriptive
+# _limit 		- Starts with an underscore
