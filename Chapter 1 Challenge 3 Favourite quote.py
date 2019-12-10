459c67e2fb74a95a60eae7bfa01c7d9774770791
koxt2 <58732688+koxt2@users.noreply.github.com>
2019-12-10 12:29:18 +0000

    Add files via upload
    # Write a program that prints your favourite quote. It should give credit to the person who said it on the next line.

---
+++ Chapter 1 Challenge 3 Favourite quote.py
@@ -0,0 +1,8 @@
+# Python programming for the Absolute Beginner, Third Edition
+# Chapter 1, Challenge 3
+# Write a program that prints your favourite quote. It should give credit to the person who said it on the next line.
+
+# Print your favourite quote
+quote = "Use sunscreen!"
+ref = "Baz Luhrmann"
+print(quote, "\n", ref)
