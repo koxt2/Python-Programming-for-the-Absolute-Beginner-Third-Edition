59b7bb08a9c25a8894c1751182480828ca0c9289
koxt2 <58732688+koxt2@users.noreply.github.com>
2019-12-10 12:28:48 +0000

    Add files via upload
    # Write and save a program that prints out your name and waits for the user to press the Enter key before the program ends.

---
+++ Chapter 1 Challenge 2 Print your name.py
@@ -0,0 +1,8 @@
+# Python programming for the Absolute Beginner, Third Edition
+# Chapter 1, Challenge 2
+# Write and save a program that prints out your name and waits for the user to press the Enter key before the program ends.
+
+# Print your name!
+name = input("What is your name?")
+print("Your name is", name)
+input("Press enter to end")
